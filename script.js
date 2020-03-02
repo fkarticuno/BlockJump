06a5776983090867cd114ff08c5176676c9d3368
Frederick Articuno <mckiyoshi@gmail.com>
2020-03-02 15:25:49 -0500

    added resting animation and punching

--- script.js
+++ script.js
@@ -5,30 +5,81 @@ var boxElement = document.getElementById('plyr');
 var player = document.getElementById('image')
 var boxLeftPos = boxElement.offsetLeft
 var boxTopPos = boxElement.offsetTop
+var orientation;
 document.getElementById("input-output").innerText = 'X: '+boxTopPos+' Y: '+boxLeftPos+' Key: N/A'
 document.addEventListener('keydown', e => {
     
-    (e.which == 38 && boxTopPos >= -5 ? playerMove(-2,'-0px -120px') : '');  //up
-    (e.which == 40 && boxTopPos <= 265 ? playerMove(2,'-00px -00px') : '');  //down
-    (e.which == 37 && boxLeftPos >= -1 ? playerMove(-1,'-0px -65px') : '');  //left
-    (e.which == 39 && boxLeftPos <= 417 ? playerMove(1,'-0px -65px') : '');  //right
-
+    (e.which == 38 && boxTopPos >= -5 ? playerMove(-2,'-0px -120px',0) : '');  //Walk U
+    (e.which == 40 && boxTopPos <= 265 ? playerMove(2,'-00px -00px',0) : '');  //Walk D
+    (e.which == 37 && boxLeftPos >= -1 ? playerMove(-1,'-0px -65px',0) : '');  //Walk L
+    (e.which == 39 && boxLeftPos <= 417 ? playerMove(1,'-0px -65px',0) : '');  //Walk R
+    (e.which == 87 ? playerMove(2,'-530px -130px',1) : '');  //Punch U
+    (e.which == 83 ? playerMove(-2,'-530px -0px',1) : ''); //Punch D
+    (e.which == 65 ? playerMove(1,'-530px -190px',1) : '');  //Punch R
+    (e.which == 68 ? playerMove(-1,'-530px -190px',1) : ''); //Punch L
+    (e.which == 32 ? toggleMusic() : '')
     document.getElementById("input-output").innerText = 'X: '+boxTopPos+' Y: '+boxLeftPos+' Key: '+e.which
     //console.log('X: ',boxTopPos, 'Y: ',boxLeftPos, 'Key:', e.which)
 })
 
-function playerMove(orientation, position) {
-    if (orientation == 1 || orientation == -1) {
+function playerMove(orientation, position, attack) {
+    // Walk
+    if ((orientation == 1 || orientation == -1) && attack == 0) {
     boxLeftPos += 30*orientation;
     boxElement.style.left = (boxLeftPos) + 'px';
     player.style.objectPosition = position
-    player.style.webkitTransform =`scaleX(${orientation})`
-    player.style.transform = `scaleX(${orientation})`
+    flipImg(orientation)
+    restL_R(orientation,'-400px -190px' )
     }
-    if (orientation == 2 || orientation == -2){
+    if ((orientation == 2 || orientation == -2) && attack == 0){
     boxTopPos += 30*orientation/2;
     boxElement.style.top = (boxTopPos) + 'px';
     player.style.objectPosition = position
+    restU_D(orientation, udSprite)
+    }
+    // Punch
+    if ((orientation == 1 || orientation == -1) && attack == 1) {
+        player.style.objectPosition = position
+        flipImg(orientation)
+        restL_R(orientation*-1, '-400px -190px')
+    }
+    if ((orientation == 2 || orientation == -2) && attack == 1) {
+        player.style.objectPosition = position
+        restU_D(orientation*-1, udSprite)
     }
     return
+}
+function flipImg(orientation) {
+    player.style.webkitTransform =`scaleX(${orientation})`
+    player.style.transform = `scaleX(${orientation})`
+}
+
+function restL_R(orientation, sprite) {
+    orientation = orientation * -1
+    sprite = sprite
+    setTimeout( ()=>{
+        player.style.objectPosition = sprite
+        flipImg(orientation)
+    },200)
+}
+var udSprite = ['-400px -130px','-400px -0px']
+function restU_D(orientation) {
+    if (orientation == 2) {
+        setTimeout( ()=>{ player.style.objectPosition = udSprite[1]},300)
+    }
+    if (orientation == -2) {
+        setTimeout( ()=>{ player.style.objectPosition = udSprite[0]},300)
+    }
+    
+}
+
+// Music
+var bgSound1 = document.getElementById('bgSound1')
+bgSound1.loop = true
+function toggleMusic() {
+    var curTime = bgSound1.currentTime
+    if (curTime > 0) {
+        bgSound1.pause()
+        bgSound1.currentTime = 0
+    } else {bgSound1.play()}
 }
